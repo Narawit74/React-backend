0a383e63b1d9ae092af370dd417b84f8b66307c5
Narawit74 <s65309010039@kktech.ac.th>
2024-02-12 16:08:32 +0700

    dsd

--- controllers/rentbook-controller.js
+++ controllers/rentbook-controller.js
@@ -21,3 +21,35 @@ exports.getByAdmin = async (req,res,next) => {
         next(err)
     }
 }
+
+exports.getByAdminSearch = async (req,res,next) => {
+    try {
+        const { text } = req.query
+        console.log(text)
+        const querytirm = text.trim()
+        if(querytirm.length === 0){
+            return res.status(405).json({message:"please fill input"})
+        }
+        const productData = await prisma.rentBook.findMany({
+            where : {
+                OR : [
+                    {
+                        Title: {
+                            contains : querytirm
+                        }
+                    }
+                ]
+            }
+        })
+        if (productData.length === 0) {
+            return res.status(404).json({ message: "No match item" });
+        }
+        
+        console.log(productData)
+    res.json({ productData })
+        
+    } catch (err) {
+        next(err)
+    }
+}
+
--- routes/rentbook-route.js
+++ routes/rentbook-route.js
@@ -5,5 +5,6 @@ const authenticate = require("../middlewares/authorization")
 
 router.get("/",authenticate,RentBookController.getByUser)
 router.get("/all",authenticate,RentBookController.getByAdmin)
+router.get("/search",authenticate,RentBookController.getByAdminSearch)
 
 module.exports = router
